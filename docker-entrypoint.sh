7db7c41a4fc176ee063f3a960c2fcbab49b92085
sgssrg <sourikghosh08@gmail.com>
2026-04-10 21:55:38 +0530

    Fixed Entrypoint file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,3 @@
-# Source - https://stackoverflow.com/a/78112472
-# Posted by Sebastian Liebscher, modified by community. See post 'Timeline' for change history
-# Retrieved 2026-04-10, License - CC BY-SA 4.0
-
 #!/bin/bash
 
 # Apply migrations
